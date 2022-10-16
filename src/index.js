934f9acdfb494a6663e6debde7e6ac32d178c8b9
nencodev <nencodev@gmail.com>
2022-10-16 02:14:49 -0400

    Entendiendo typeof

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const text = "Hola, me llamo Manz";
+const number = 42;
+const boolean = true;
+let notDefined;
+
+console.log(typeof text);
+console.log(typeof number);
+console.log(typeof boolean);
+console.log(typeof notDefined);
