a2d8ba270f4a4a66e42ab4baad1733b76e240e84
nencodev <nencodev@gmail.com>
2022-10-17 00:54:41 -0400

    Agregado Operadores Lógicos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-const text = "Hola, me llamo Manz";
-const number = 42;
-const boolean = true;
-let notDefined;
-
-console.log(typeof text);
-console.log(typeof number);
-console.log(typeof boolean);
-console.log(typeof notDefined);
+const x = 1;
+const y = "OK";
+const oper = "&&";
+console.log("x =", x, " ", "y =", y);
+console.log("x", oper, "y");
+console.log(Boolean(x), oper, Boolean(y), "devuelve", Boolean(x && y));
+console.log(x, oper, y, "devuelve", "(", (x && y), ")");
